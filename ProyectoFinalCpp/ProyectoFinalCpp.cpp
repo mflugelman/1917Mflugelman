5883ef1345034d94d1f94ce0116b5356d570f5da
Maximo Flugelman <mflugelman@binit.dev>
2022-11-25 20:07:53 -0300

    added sfml

--- ProyectoFinalCpp/ProyectoFinalCpp.cpp
+++ ProyectoFinalCpp/ProyectoFinalCpp.cpp
@@ -1,20 +1,42 @@
 // ProyectoFinalCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    constexpr int WINDOW_WIDTH = 800;
+    constexpr int WINDOW_HEIGHT = 600;
+
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "1917");
+
+    sf::Vector2f trench(WINDOW_WIDTH, 50);
+
+    sf::RectangleShape home_base(trench);
+    home_base.setFillColor(sf::Color::Green);
+    home_base.setPosition(0, WINDOW_HEIGHT - 50);
+
+    sf::RectangleShape enemy_base(trench);
+    enemy_base.setFillColor(sf::Color::Red);
+    enemy_base.setPosition(0, 0);
+
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(home_base);
+        window.draw(enemy_base);
+        window.display();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
