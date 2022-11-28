a1eaf043037d9361abd82693b968fdb3531f885d
Maximo Flugelman <mflugelman@binit.dev>
2022-11-28 10:59:44 -0300

    created player entities added textures and background

---
+++ ProyectoFinalCpp/Player.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+#include "Unit.h"
+
+using namespace std;
+
+class Player
+{
+public:
+	Player();
+	~Player();
+
+	int getHealth();
+	void setHealth(int health);
+
+	int getMoney();
+	void setMoney(int money);
+
+	void addUnit(shared_ptr<Unit> unit);
+
+	void setBase(int width, int height, int posX, int posY);
+	sf::RectangleShape getBase();
+	vector<shared_ptr<Unit>> m_units;
+
+private:
+	int m_money;
+	int m_health;
+	static const string s_baseTexture;
+
+	sf::RectangleShape m_base;
+	sf::Texture* m_texture;
+};
